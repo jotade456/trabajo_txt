e480437bd98f37e441418242c797aa9b543c82df
jotade456 <diazrojas7890@gmail.com>
2024-07-28 12:31:32 -0500

    Update controlador_txt.py
    controlador definitivo de creacion de archivo txt, debe estar en una carpeta para permitir la importacion

--- controlador_txt.py
+++ controlador_txt.py
@@ -1,16 +1,21 @@
-import json
+from vistaa import Vista
+from modelo_txt import archivo
 class controlador:
     def __init__(self):
-        self.objModelo=modelo()
-        self.objVista=vista()
+        self.objModelo=archivo()
+        self.objVista=Vista(self)
         
-    def guardar_texto(self):
-        texto=self.objVista.tomarTexto()
-        texto=self.objModelo.guardarTexto(texto)
-        texto=self.objModelo.crearArchivo(texto)
+    def guardarDatos(self,datoTexto,nombreArchivo):
+        texto=self.objModelo.guardarTexto(datoTexto)
+        nombreArchivo=self.objModelo.crearArchivo(texto,nombreArchivo)
+        return nombreArchivo
+        
+    def deserializar (self,nombreArchivo):
+        texto=self.objModelo.deserializar(nombreArchivo)
         return texto
     
-    def crearArchivo(self,datoTexto):
-        nombreArchivo=self.objVista.tomarNombre()
-        nombreArchivo=self.objModelo.crearArchivo()
-        return nombreArchivo
+    def ejecutar(self):
+        self.objVista.iniciar()
+        
+objEjecutar=controlador()
+objEjecutar.ejecutar()
